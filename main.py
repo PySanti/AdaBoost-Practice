e289cdc4b774ecbeb94db6e7b495d7221181aae4
santiago <santiagodeandrade4@gmail.com>
2025-03-12 14:00:45 -0400

    Avances en preprocesamiento tanto con outliers, scalers y pca

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ from utils.basic_preprocess import basic_preprocess
 
 
 target = "HeartDisease"
-[df_train, df_val, df_test] = basic_preprocess(pd.read_csv("./data/data.csv"), target)
-print(df_train.head(5))
-print(df_val.head(5))
-print(df_val.head(5))
+[df_train, df_test, df_val] = basic_preprocess(pd.read_csv("./data/data.csv"), target, outliers=False, scaler=True, pca=True)
+
 
